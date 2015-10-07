2a46e7dad28098f48389caf313cfea4232310baa
Cathysia <egalite@qq.com>
2015-10-07 13:14:17 +0800

    I added some new files

---
+++ multiplication.py
@@ -0,0 +1,8 @@
+for a in range(1, 10):
+ for b in range(1, a+1):
+  print b, '*', a, '=', a*b,
+ print '\n'
+
+print r"I didn't know how to seperate lines goddamn it."
+print 'I hate you!'
+print 'bark...'
